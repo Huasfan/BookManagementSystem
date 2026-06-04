6cbc0cb7510f08c99affe627a38571d2408aa0d8
Huasfan <Huasfan811@gmail.com>
2026-06-04 09:18:41 +0800

    修复首页模板与视图：修正查询字段名、增加内容边距、渲染借书记录

--- bookmanagement/views.py
+++ bookmanagement/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from bookmanagement.models import Borrows
 
-def index (request):
-    return render(request, 'index.html')
+def index(request):
+    borrows = Borrows.objects.filter(returntype=0).values("username__username", "book__bookname", "borrowdate")
+    return render(request, 'index.html', {'borrows': borrows})
+
