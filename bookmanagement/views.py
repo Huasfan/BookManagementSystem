4b1e3e093c6e665fb08e9d2d66074d83070865a4
Huasfan <Huasfan811@gmail.com>
2026-06-04 09:35:57 +0800

    添加种子数据并完善首页表格：显示全部借书记录及归还状态

--- bookmanagement/views.py
+++ bookmanagement/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from bookmanagement.models import Borrows
 
 def index(request):
-    borrows = Borrows.objects.filter(returntype=0).values("username__username", "book__bookname", "borrowdate")
+    borrows = Borrows.objects.values("username__username", "book__bookname", "borrowdate", "returntype")
     return render(request, 'index.html', {'borrows': borrows})
 
